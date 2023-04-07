eeeda59483aa902ae8331fb643df31f696524a47
AlexeyLepov <77492646+AlexeyLepov@users.noreply.github.com>
2023-04-07 03:03:07 +0300

    include different colors to the fire

--- SimpleFireGen/SimpleFireGen.cpp
+++ SimpleFireGen/SimpleFireGen.cpp
@@ -17,10 +17,12 @@
 ////////////////////////////////////////////////////////////////////////////
 
 
+
 #include <iostream>
 #include <chrono>
 #include <thread>
 #include <fstream>
+
 
 
 using namespace std;
@@ -31,8 +33,33 @@ const int HEIGHT = 32;
 const int NUM_SYMBOLS = 16;
 
 
+
 int fire[HEIGHT][WIDTH] = { 0 };
-char symbolsArr[NUM_SYMBOLS] = { ' ', '.', ':', '\'', '\"', ';', '^', '*', '+', '=', 'x', '%', '&', '@', '$', '#'};
+char symbolsArr[NUM_SYMBOLS] = { ' ', '.', ':', '\'', '\"', ';', '^', '*', '+', '=', 'x', '%', '&', '@', '$', '#' };
+
+
+
+////////////////////////////////////////////////////////////////////////////
+// check your OS
+////////////////////////////////////////////////////////////////////////////
+int defOS()
+{
+    /* Windows OS is selected as default */
+    int os = 0;
+    ofstream f_wind("/usr/lib/systemd");
+    /* if OS is Unix based */
+    if (f_wind.is_open())
+    {
+        os = 1;
+    }
+    /* if OS is Windows */
+    else
+    {
+        os = 0;
+    }
+    return os;
+}
+
 
 
 void initializeFire()
@@ -43,6 +70,8 @@ void initializeFire()
         fire[HEIGHT - 1][i] = NUM_SYMBOLS - 1;
     }
 }
+
+
 
 void updateFire()
 {
@@ -62,18 +91,102 @@ void updateFire()
     }
 }
 
+
+
 void renderFire()
 {
+    ////////////////////////////////////////////////////////////////////////////
+    // simple rendering
+    ////////////////////////////////////////////////////////////////////////////
+    //for (int y = 0; y < HEIGHT; y++)
+    //{
+    //    for (int x = 0; x < WIDTH; x++)
+    //    {
+    //        cout << symbolsArr[fire[y][x]];
+    //    }
+    //    cout << endl;
+    //}
+
+    ////////////////////////////////////////////////////////////////////////////
+    // colored rendering
+    ////////////////////////////////////////////////////////////////////////////
+    // Defineing 16 intensity levels and associating each level with a different color using ANSI escape codes. 
+    // Printing each symbol with the appropriate color and reseting the color at the end of each row. 
+    // The ;1 modifier makes the "Dark Gray" text bold.
     for (int y = 0; y < HEIGHT; y++)
     {
         for (int x = 0; x < WIDTH; x++)
         {
-            cout << symbolsArr[fire[y][x]];
+            int intensity = fire[y][x];
+            if (intensity == 0) {
+                // Black
+                cout << "\033[30m";
+            }
+            else if (intensity < 2) {
+                // Dark Blue
+                cout << "\033[34m";
+            }
+            else if (intensity < 3) {
+                // Light Blue
+                cout << "\033[94m";
+            }
+            else if (intensity < 4) {
+                // Dark Green
+                cout << "\033[32m";
+            }
+            else if (intensity < 5) {
+                // Light Green
+                cout << "\033[92m";
+            }
+            else if (intensity < 6) {
+                // Dark Cyan
+                cout << "\033[36m";
+            }
+            else if (intensity < 7) {
+                // Light Cyan
+                cout << "\033[96m";
+            }
+            else if (intensity < 8) {
+                // Dark Red
+                cout << "\033[31m";
+            }
+            else if (intensity < 9) {
+                // Light Red
+                cout << "\033[91m";
+            }
+            else if (intensity < 10) {
+                // Dark Magenta
+                cout << "\033[35m";
+            }
+            else if (intensity < 11) {
+                // Light Magenta
+                cout << "\033[95m";
+            }
+            else if (intensity < 12) {
+                // Dark Yellow
+                cout << "\033[33m";
+            }
+            else if (intensity < 13) {
+                // Light Yellow
+                cout << "\033[93m";
+            }
+            else if (intensity < 14) {
+                // Dark Gray
+                cout << "\033[30;1m";
+            }
+            else if (intensity < 15) {
+                // Light Gray
+                cout << "\033[37;1m";
+            }
+            else {
+                // White
+                cout << "\033[97;1m";
+            }
+            cout << symbolsArr[intensity];
         }
-        cout << endl;
+        cout << "\033[0m" << endl; // Reset color
     }
 }
-
 
 
 
@@ -82,14 +195,28 @@ void renderFire()
 ////////////////////////////////////////////////////////////////////////////
 int main()
 {
+    system("Color 06");
     initializeFire();
 
-    while (true)
+    if (defOS() == 0)
     {
-        updateFire();
-        renderFire();
-        this_thread::sleep_for(chrono::milliseconds(50));
-        if (system("CLS")) system("clear"); // Clear the console
+        while (true)
+        {
+            updateFire();
+            renderFire();
+            this_thread::sleep_for(chrono::milliseconds(50));
+            system("cls"); // Clear the console
+        }
+    }
+    else
+    {
+        while (true)
+        {
+            updateFire();
+            renderFire();
+            this_thread::sleep_for(chrono::milliseconds(50));
+            system("clear"); // Clear the console
+        }
     }
 
     return 0;
