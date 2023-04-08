a0790b6857b58e044b88a67a2f75fa6406a21d1b
AlexeyLepov <77492646+AlexeyLepov@users.noreply.github.com>
2023-04-08 19:52:14 +0300

    4 intensity levels for red color only

--- SimpleFireGen/SimpleFireGen.cpp
+++ SimpleFireGen/SimpleFireGen.cpp
@@ -113,74 +113,35 @@ void renderFire()
     // Defineing 16 intensity levels and associating each level with a different color using ANSI escape codes. 
     // Printing each symbol with the appropriate color and reseting the color at the end of each row. 
     // The ;1 modifier makes the "Dark Gray" text bold.
+    int intensityLevels = HEIGHT / 4;
     for (int y = 0; y < HEIGHT; y++)
     {
         for (int x = 0; x < WIDTH; x++)
         {
             int intensity = fire[y][x];
             if (intensity == 0) {
-                // Black
-                cout << "\033[30m";
-            }
-            else if (intensity < 2) {
-                // Dark Blue
-                cout << "\033[34m";
-            }
-            else if (intensity < 3) {
-                // Light Blue
-                cout << "\033[94m";
-            }
-            else if (intensity < 4) {
-                // Dark Green
-                cout << "\033[32m";
-            }
-            else if (intensity < 5) {
-                // Light Green
-                cout << "\033[92m";
-            }
-            else if (intensity < 6) {
-                // Dark Cyan
-                cout << "\033[36m";
-            }
-            else if (intensity < 7) {
-                // Light Cyan
-                cout << "\033[96m";
-            }
-            else if (intensity < 8) {
                 // Dark Red
                 cout << "\033[31m";
             }
-            else if (intensity < 9) {
-                // Light Red
-                cout << "\033[91m";
-            }
-            else if (intensity < 10) {
-                // Dark Magenta
-                cout << "\033[35m";
-            }
-            else if (intensity < 11) {
-                // Light Magenta
-                cout << "\033[95m";
-            }
-            else if (intensity < 12) {
-                // Dark Yellow
-                cout << "\033[33m";
-            }
-            else if (intensity < 13) {
+            else if (intensity < intensityLevels/2) {
                 // Light Yellow
                 cout << "\033[93m";
             }
-            else if (intensity < 14) {
-                // Dark Gray
-                cout << "\033[30;1m";
+            else if (intensity < intensityLevels) {
+                // Dark Yellow
+                cout << "\033[33m";
             }
-            else if (intensity < 15) {
-                // Light Gray
-                cout << "\033[37;1m";
+            else if (intensity < intensityLevels * 1.5) {
+                // Light Red
+                cout << "\033[91m";
+            }
+            else if (intensity < intensityLevels * 2) {
+                // Dark Red
+                cout << "\033[31m";
             }
             else {
                 // White
-                cout << "\033[97;1m";
+                cout << "\033[97m";
             }
             cout << symbolsArr[intensity];
         }
